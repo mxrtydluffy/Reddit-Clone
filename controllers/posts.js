f70a921baaacb8c8358bc8ac3fcf9281491f2ac7
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-05-04 19:03:02 -0700

    [UPDATE] Users can see Subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,35 +1,42 @@
-const post = require('../models/post');
-const Post = require('../models/post');
+const Post = require("../models/post");
 
  module.exports = (app) => {
-   app.post("/", (req, res) => {
-    Post.find({}).lean()
-    .then((posts) => res.render('posts-index', { posts }))
-    .catch((err) => {
+   app.get("/", (req, res) => {
+    Post.find({})
+      .lean()
+      .then((posts) => res.render('posts-index', { posts }))
+      .catch((err) => {
       console.log(err.message);
-    })
+    });
   });
 
   // CREATE
-  app.get('/post/new', (req, res) => {
+  app.get("/post/new", (req, res) => {
     res.render("post-new");
   });
 
-  app.post('/post/new', (req, res) => {
+  app.post("/post/new", (req, res) => {
     console.log(req.body);
     const post = new Post(req.body);
 
-    post.save()
+    post
+      .save()
       .then(() => res.redirect('/'))
       .catch(err => console.log(err));
   })
 
   // DISPLAY
-  app.get('/posts/:id', (req, res) => {
-    Post.findById(req.params.id).lean()
-    .then((post) => res.render('posts-show', { post }))
+  app.get("/posts/:id", (req, res) => {
+    Post.findById(req.params.id)
+    .lean()
+    .then((post) => res.render("posts-show", { post }))
     .catch((err) => {
       console.log(err.message)
-    })
-  });   
- };
+    });
+  });
+  
+  // SUBREDDIT
+  app.get("/n/:subreddit", (req, res) => {
+    console.log(req.params.subreddit);
+  }); 
+};
